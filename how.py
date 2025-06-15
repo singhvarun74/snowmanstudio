dc30d2506722e9727ba67ff62e0d0e5256405a91
Panda <varun.ds0551@gmail.com>
2025-06-15 18:42:34 +0530

    update 2 game.json and how.py

---
+++ how.py
@@ -0,0 +1,12 @@
+#to add games data use this template
+''' {
+        "id": "no of game",
+        "title": "its name",
+        "imageUrl": "image url",
+        "imageHint": "img description",
+        "itchioPageUrl": "itch.io link",
+        "description": "about game",
+        "isFeatured": false or true,
+    },
+    '''
+
